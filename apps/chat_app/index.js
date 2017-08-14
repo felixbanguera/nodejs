21d91afdfe98922ca92c4aba0c7d1c76bd3993ad
Your Namefelixbanguera <jorge.banguera@yuxiglobal.com>
2017-08-14 08:54:08 -0500

    initialize a new instance of socket.io by passing the http (the HTTP server) object. Then I listen on the connection event for incoming sockets, and I log it to the console.  load the socket.io-client, which exposes a io global, and then connect.

--- apps/chat_app/index.js
+++ apps/chat_app/index.js
@@ -5,17 +5,25 @@ This translates into the following:
 - We define a route handler / that gets called when we hit our website home.
 - We make the http server listen on port 3000.
 
+******
+- Notice that I initialize a new instance of socket.io by passing the http (the HTTP server) object.
+- Then I listen on the connection event for incoming sockets, and I log it to the console.
+
 to make it run: >> node index.js
 */
 
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  // res.send('<h1>Hello world</h1>');
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket){
+  console.log('A user connected????');
+});
+
 http.listen(3333, function(){
-  console.log('listening on *:3333');
+  console.log('Listening on *:3333');
 });
