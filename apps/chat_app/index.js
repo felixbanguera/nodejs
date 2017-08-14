f0389a0b5270648ccd49496d872dafcb7c4bb2df
Your Namefelixbanguera <jorge.banguera@yuxiglobal.com>
2017-08-14 09:02:03 -0500

    when the user types in a message, the server gets it as a chat message event. print out the chat message event.

--- apps/chat_app/index.js
+++ apps/chat_app/index.js
@@ -22,6 +22,9 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   console.log('A user connected????');
+  socket.on('chat-message', function(msg){
+    console.log('message: ' + msg);
+  });
   socket.on('disconnect', function(){
     console.log('A user disconnected');
   });
