ad1886f49deff5ce67a6ac57eaf66032ddac31fd
Your Namefelixbanguera <jorge.banguera@yuxiglobal.com>
2017-08-14 08:29:44 -0500

    Created first index.js version to initialize app to be a function handler to be supplied an http server

---
+++ apps/chat_app/index.js
@@ -0,0 +1,20 @@
+/*
+This translates into the following:
+
+- Express initializes app to be a function handler that you can supply to an HTTP server (as seen in line 2).
+- We define a route handler / that gets called when we hit our website home.
+- We make the http server listen on port 3000.
+
+to make it run: >> node index.js
+*/
+
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(3333, function(){
+  console.log('listening on *:3333');
+});
