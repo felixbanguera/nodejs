6b487b0fd8a9458fd96bb679c1c23c9139a2a0c6
Your Namefelixbanguera <jorge.banguera@yuxiglobal.com>
2017-08-18 14:04:59 -0500

    Copied the basic js exmaple from webworker-thread lib. Ccreated a test implementation of inifinite loop running on one thread

---
+++ excercises/libs_tests/node_webworker_threads/infinite_thread_test_1.js
@@ -0,0 +1,38 @@
+/*
+Here the testing the use of pools to create an Infinite loop
+*/
+function infinite_loop(i, continuu=true){
+  console.log('infinite_loop');
+  return true;
+}
+
+function run_IL(){
+  console.log('in run_IL...');
+  thread.on('run', function(i){
+    console.log(`run_IL::run, ${i}`);
+      thread.emit('back', infinite_loop(i));
+  });
+  thread.on('stoppppp', function(i){
+    console.log("Stopppppped loop", i);
+  });
+}
+
+var numThreads= 1;
+console.log("Using "+ numThreads+ " threads.");
+var Worker= require('webworker-threads');
+var tread_worker = Worker.create();
+// var tread_worker= Worker.createPool(numThreads)
+tread_worker.eval(infinite_loop);
+tread_worker.eval(run_IL);
+
+tread_worker.on('back', function(res){
+  console.log(`In the back event with value: ${res}`);
+  tread_worker.emit('run', 1);
+})
+
+tread_worker.eval('run_IL()', function(err, result) {
+  if (err) throw err;
+  console.log("run_IL() is done!");
+});
+
+tread_worker.emit('run', 0);
