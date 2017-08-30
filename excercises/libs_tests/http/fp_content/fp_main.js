20959bf7eb51db100dde558c4840288759f718bc
Laura <laura.ciro@yuxiglobal.com>
2017-08-30 08:55:56 -0500

    delete spaces

--- excercises/libs_tests/http/fp_content/fp_main.js
+++ excercises/libs_tests/http/fp_content/fp_main.js
@@ -8,20 +8,13 @@ In this file:
 // To communicate through events???
 var fs = require('fs');
 var events = require('events');
-
-
-
 var webiopi = require('../webiopi.js');
 var utils = require('./fp_utils.js');
 
 var eventEmitter = new events.EventEmitter();
 
-
-
 // Get the devices and config data
 var fp_hw = JSON.parse(fs.readFileSync(__dirname+'/fp_hw.json', 'utf8'));
-
-
 
 // *****Configure all devices GPIO*****
 // *****Save all devices statuses*****
@@ -30,8 +23,6 @@ utils.do.configure_all_devices(Object.entries(fp_hw.devices));
 
 // Run sensors validations
 // Subscribe to external events to post to webiopi
-
-
 
 function basic_callback(status, body){
   console.log(`Response: status code: ${status} and body: ${body}`);
