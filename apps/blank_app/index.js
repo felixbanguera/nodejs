75f888a05c526a900e78240b566533bb63406c9c
Your Namefelixbanguera <jorge.banguera@yuxiglobal.com>
2017-08-31 10:14:05 -0500

    Manually Using an RX.subject through callback for http request. Also downloaded the rx-node lib to use later.

--- apps/blank_app/index.js
+++ apps/blank_app/index.js
@@ -6,6 +6,16 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/getty', function(req, res){
+  console.log('Inside get call in Node server...');
+  res.end("{'a':123}");
+});
+
+app.post('/posty', function(req, res){
+  console.log('Inside post call in Node server...');
+  res.end("HOla posty!");
+});
+
 http.listen(4444, function(){
   console.log('Listening on *:4444');
 });
---
+++ excercises/libs_tests/rxjs/index.js
@@ -0,0 +1,29 @@
+/*
+Will try to achieve the use of rxjs for http requests to avoid using callbacks
+
+In this file:
+
+*/
+const Rx = require('rx');
+
+const http_com = require('./http_com.js');
+
+const postData = "\{'msg': 'Hello World!'\}";
+
+const subject = new Rx.Subject();
+
+
+// This first attempt is to manually use rxjs sendig the RX.subject through callback fuction
+console.log('Running http_get to balnk app');
+var req_get = http_com.get.request_with_callback('local_blank_app', '/getty', null, null, subject_callback);
+req_get.write(postData);
+req_get.end();
+
+subject.subscribe((data) => {
+  console.log(`from subject_callback status: ${data.status}, body: ${data.body}`);
+});
+
+function subject_callback(status, body){
+  subject.onNext({status: status, body: body});
+};
+
