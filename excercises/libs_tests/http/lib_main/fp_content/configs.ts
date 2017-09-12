e8b220dcb1a0b048e88055ca4e76a6bffd077c91
Laura <laura.ciro@yuxiglobal.com>
2017-09-12 11:26:38 -0500

    refactor

--- excercises/libs_tests/http/lib_main/fp_content/configs.ts
+++ excercises/libs_tests/http/lib_main/fp_content/configs.ts
@@ -2,8 +2,6 @@ import  *  as fs from 'fs';
 import  *  as low from 'lowdb/lib/main';
 import  *  as FileSync from 'lowdb/adapters/FileSync';
 
-
-
 interface hw_config{
   type: string,
   ip: string,
@@ -17,7 +15,7 @@ export class Configs {
   fp_hw:Array<hw_config>;
   fp_id:Array<id_config>;
   mix:any;
-  existFilesID: any; 
+  dbConnIDs: any; 
   constructor() {
     this.fp_hw = this.readJson('/fp_hw.json');
     this.mix = this.fp_id = this.readJson('/fp_id.json');
@@ -34,14 +32,15 @@ export class Configs {
   private readJson(URI){
     return JSON.parse(fs.readFileSync(__dirname + URI, 'utf8'));
   }
+
   private setDevIdsExist(){
-    this.existFilesID = {};
+    this.dbConnIDs = {};
     Object.entries(this.fp_hw).filter(([key, data])=>{
       return (fs.existsSync(`${__dirname}/devices_status/${key}.json`));
     }).forEach(([key]) => {
       const adapter = new FileSync(`${__dirname}/devices_status/${key}.json`);
       const db= low(adapter);
-       Object.assign(this.existFilesID,{ [key] : db});
+      this.dbConnIDs[key] = db;
     });
     
   }
--- excercises/libs_tests/http/lib_main/fp_content/fp_persist_device.ts
+++ excercises/libs_tests/http/lib_main/fp_content/fp_persist_device.ts
@@ -3,15 +3,14 @@ import {Configs} from './configs.js';
 export class PersistDevice{
   conf;
   constructor(){
-     this.conf = new Configs();
+    this.conf = new Configs();
   }
   
   // get device all  status from file
   getDevAllStatus(dev_id){
     let statusData = {}
-    if(Object.entries(this.conf.existFilesID).some(([devId, configData]) => devId === dev_id)){
-
-      statusData =  this.conf.existFilesID[dev_id].getState();
+    if(Object.entries(this.conf.dbConnIDs).some(([devId, configData]) => devId === dev_id)){
+      statusData =  this.conf.dbConnIDs[dev_id].getState();
     }else{
       console.log(`getDevAllStatus:: file for device id ${dev_id} don't exist `);
     }
@@ -19,10 +18,7 @@ export class PersistDevice{
   }
 
   save_new_state(dev_id, newData){
-    console.log('save_new_state');
-    this.conf.existFilesID[dev_id].setState(newData).write();
-    // fs.writeFile(`${__dirname}/devices_status/${dev_id}.json`, JSON.stringify(newData, null, 4),
-    // (err)=> console.log(err));
+    this.conf.dbConnIDs[dev_id].setState(newData).write();
   }
   
 }
