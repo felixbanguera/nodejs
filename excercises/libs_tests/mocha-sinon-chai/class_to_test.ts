ddd313940d27d844ca6d019822df51ff91f81dda
Your Namefelixbanguera <jorge.banguera@yuxiglobal.com>
2017-09-22 14:57:30 -0500

    Added couple of teste

--- excercises/libs_tests/mocha-sinon-chai/class_to_test.ts
+++ excercises/libs_tests/mocha-sinon-chai/class_to_test.ts
@@ -1,10 +1,10 @@
-// import 'reflect-metadata'
+import 'reflect-metadata'
 // This class will contain all types of methods to be tested in Middleware
 export class ClassToTest {
 
   constructor(){
     // To be used to test private functions
-    // Reflect.defineMetadata("private_method", this.private_method, ClassToTest.prototype, "private_method");
+    Reflect.defineMetadata("private_method", this.fn_private, ClassToTest.prototype, "private_method");
   }
 
   fn_returns_obj(){
@@ -13,6 +13,17 @@ export class ClassToTest {
 
   fn_method_exists(){
     return "console.log('WHAAATTT');";
+  }
+
+  fn_returns_nothing(){
+  }
+
+  fn_with_parameters(arg1:Boolean){
+    console.log(`arg1: ${arg1}`);
+  }
+
+  private fn_private(){
+    return true;
   }
 
 }
--- excercises/libs_tests/mocha-sinon-chai/test/test_to_class.ts
+++ excercises/libs_tests/mocha-sinon-chai/test/test_to_class.ts
@@ -6,28 +6,22 @@ var assert = chai.assert;
 // if you used the '@types/mocha' method to install mocha type definitions, uncomment the following line
 import 'mocha';
 
+// Tests for methods returning an object
 describe('ClassToTest.fn_returns_obj() function', () => {
-  // it('should return an object', () => {
-  //   const _ClassToTest = new ClassToTest();
-  //   const metadataValue = Reflect.getMetadata("private_method", _ClassToTest, "private_method");
-  //   const result = metadataValue();
-  //   assert.typeOf(result, 'Object');
-  //   expect(result).to.be.a('Object');
-  //   result.should.be.a('Object');
-  // });
 
+  // Test it should return an object
   it('should return an object', () => {
     const _ClassToTest = new ClassToTest();
     const result = _ClassToTest.fn_returns_obj();
     assert.typeOf(result, 'Object');
   });
-
+  // test the return should contain a specific key
   it('should return an object with key : vero', () => {
     const _ClassToTest = new ClassToTest();
     const result = _ClassToTest.fn_returns_obj();
     assert.property(result, 'vero');
   });
-
+  // test a value of a specific key is of a specific type
   it('should return an object with key : vero and its value to be a string', () => {
     const _ClassToTest = new ClassToTest();
     const result = _ClassToTest.fn_returns_obj();
@@ -36,9 +30,47 @@ describe('ClassToTest.fn_returns_obj() function', () => {
 
 });
 
+// Test a method exists
 describe('ClassToTest.fn_method_exists() function', () => {
   it('should exist', () => {
     const _ClassToTest = new ClassToTest();
     assert.exists(_ClassToTest['fn_method_exists'], '_ClassToTest.fn_method_exists is neither `null` nor `undefined`');
   });
 });
+
+// test a return is undefined, for method that shouldn't return something, should this be tested?
+describe('ClassToTest.fn_returns_nothing() function', () => {
+  it('should not return anything', () => {
+    const _ClassToTest = new ClassToTest();
+    assert.isUndefined(_ClassToTest.fn_returns_nothing(), '_ClassToTest.fn_returns_nothing is returning a value, and should not');
+  });
+});
+
+// Test a method exists, Typescript itself verifies that.
+// test a method with parameters, to receive a parameter is being tested by Typescript as well as parameter types
+
+// HOw to test a private method's functionality
+// There are two ways, a simple, not sure why way:
+//   Create an instance of the class and the access the method throug squer brakets:
+//     _ClassToTest['fn_private']()
+// The second one is to use reflection, but as Javascript doesn't support it, the a lib should be imported:
+// 1. You need to use 'Reflect-metadata' in the class and somewhere set the function as metadata
+// 2. In the description of the test you need to get the metadata instance
+describe('ClassToTest.fn_private() function', () => {
+  // The first method
+  it('should return true using square brakets', () => {
+    const _ClassToTest = new ClassToTest();
+    const metadataValue = Reflect.getMetadata("private_method", _ClassToTest, "private_method");
+    const result = _ClassToTest['fn_private']();
+    assert.equal(result, true);
+  });
+
+  it('should return true using reflection', () => {
+    const _ClassToTest = new ClassToTest();
+    const metadataValue = Reflect.getMetadata("private_method", _ClassToTest, "private_method");
+    const result = metadataValue();
+    assert.equal(result, true);
+  });
+});
+
+
